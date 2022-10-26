fc0bf5eb8a05a68f2fbf9d517a30c9f1301f6549
panwan1040 <6206021621040@fitm.kmutnb.ac.th>
2022-10-26 15:25:32 +0700

    งงว่ะ

--- test_sent_cut.py
+++ test_sent_cut.py
@@ -1,1 +1,35 @@
 
+
+def mysent_tokenize(text):
+    santhan = getsanthan("./asset/santhan.txt")
+    text = text.split(" ")
+    listsent = []
+    for i in text:
+        # text เป็น "เล่าเหตุการณ์ที่เกิดขึ้นว่า"
+        for san in santhan:
+            # print(i.split(san))
+            # เชื่อม ประโยคหลัง
+            if i.split(san)[len(i.split(san))-1] == "":
+                if i != "":
+                    print(i)
+
+            # เชื่อม ประโยคหน้า
+            if i.split(san)[0] == "":
+                if i != "":
+                    print(i)
+
+
+def getsanthan(pathfile):
+    f = open(pathfile, "r", encoding="utf-8")
+    return f.read().split("\n")
+
+
+# mysent_tokenize("xx")
+
+
+mysent_tokenize(
+    """ณ บึงใหญ่ในป่าแห่งหนึ่ง ห่านและนกกระสาชวนกันเดินท่องน้ำหาปลาเล็กปลาน้อยกินเป็นอาหาร สัตว์ทั้งสองไม่รู้ตัวเลยว่าเบื้องหลังพุ่มไม้ที่อยู่ริมบึงนั้น
+
+            พรานป่าคนหนึ่งยืนถือปืนคอยท่าจะยิงพวกมันอยู่ ในขณะที่นกกระสาค่อยๆ จับปลากินอย่างไม่รีบร้อนนั้น ห่านกลับก้มหน้าก้มตากินอย่างไม่ยั้ง เมื่อนกกระสาหันมาเห็น มันจึงพูดเดือนขึ้นว่า 
+
+            นี่ท่าน ถ้ากินมากขนาดนี้ระวังบินไม่ไหวนะ""")
