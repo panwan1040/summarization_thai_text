1bf937fb7cfae96e518fe9acf83febdbde7f9ef2
panwan1040 <6206021621040@fitm.kmutnb.ac.th>
2022-10-31 13:51:20 +0700

    จัดใหม่

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     def tokez(text):
         text = clean_msg(text)
         # ตัดประโยค โดยใช้ วรรคและขึ้นบรรทัดใหม่
-        text = sent_tokenize(text, engine="whitespace+newline")
+        text = mysent_tokenize(text)
         # text = mysent_tokenize(text)
         for i in text:
             newtext = word_tokenize(i, engine='mm')
@@ -80,60 +80,58 @@ if __name__ == '__main__':
             tmp = dictionary.get(term_id), weight
             result.append(tmp)
         print(result)
+        return result
 
+    def mysent_tokenize(text):
+        santhan = getsanthan("./asset/santhan.txt")
+        text = text.split(" ")
+        # listsent = []
+        index = 0
+        # print(text)
+        for i in text:
+            tmplist = ""
+            # for สำหรับจัดการคำหลังสุดที่เป็นคำสันธานให้เชื่อมกับประโยคถัดไป
+            for san in santhan:
+                # ถ้าประโยคหลังเป็นคำสันธาน
+                if i.split(san)[len(i.split(san))-1] == "":
+                    if i != "":
+                        # print(i)
+                        # เชื่อมประโยคถัดไป
+                        tmplist = i + " " + text[index+1]
+                        text[index] = ""
+                        text[index+1] = tmplist
+            index += 1
+        index = 0
+        for i in text:
+            tmplist = ""
+            # for สำหรับจัดการคำหน้าสุดที่เป็นคำสันธานให้เชื่อมกับประโยคก่อนหน้า
+            for san in santhan:
+                # ถ้าประโยคหน้าเป็นคำสันธาน
+                if i.split(san)[0] == "":
+                    if i != "":
+                        tmplist = text[index-1] + " " + i
+                        # print(tmplist)
+                        text[index-1] = ""
+                        text[index] = tmplist
+            # listsent.append(tmplist)
+            index += 1
+        return list(filter(None, text))
 
-def mysent_tokenize(text):
-    santhan = getsanthan("./asset/santhan.txt")
-    text = text.split(" ")
-    # listsent = []
-    index = 0
-    # print(text)
-    for i in text:
-        tmplist = ""
-        # for สำหรับจัดการคำหลังสุดที่เป็นคำสันธานให้เชื่อมกับประโยคถัดไป
-        for san in santhan:
-            # ถ้าประโยคหลังเป็นคำสันธาน
-            if i.split(san)[len(i.split(san))-1] == "":
-                if i != "":
-                    # print(i)
-                    # เชื่อมประโยคถัดไป
-                    tmplist = i + " " + text[index+1]
-                    text[index] = ""
-                    text[index+1] = tmplist
-        index += 1
-    index = 0
-    for i in text:
-        tmplist = ""
-        # for สำหรับจัดการคำหน้าสุดที่เป็นคำสันธานให้เชื่อมกับประโยคก่อนหน้า
-        for san in santhan:
-            # ถ้าประโยคหน้าเป็นคำสันธาน
-            if i.split(san)[0] == "":
-                if i != "":
-                    tmplist = text[index-1] + " " + i
-                    # print(tmplist)
-                    text[index-1] = ""
-                    text[index] = tmplist
-        # listsent.append(tmplist)
-        index += 1
-    return list(filter(None, text))
+    def getsanthan(pathfile):
+        f = open(pathfile, "r", encoding="utf-8")
+        return f.read().split("\n")
 
+    def uniquelist(list1):
 
-def getsanthan(pathfile):
-    f = open(pathfile, "r", encoding="utf-8")
-    return f.read().split("\n")
+        # initialize a null list
+        unique_list = []
 
-
-def uniquelist(list1):
-
-    # initialize a null list
-    unique_list = []
-
-    # traverse for all elements
-    for x in list1:
-        # check if exists in unique_list or not
-        if x not in unique_list:
-            unique_list.append(x)
-    # print list
-    return unique_list
+        # traverse for all elements
+        for x in list1:
+            # check if exists in unique_list or not
+            if x not in unique_list:
+                unique_list.append(x)
+        # print list
+        return unique_list
 
     main()
